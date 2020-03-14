71c2cc8dd06c2e022e464c7e32dc96b0e5182e54
Teo Min Si <minsi.teo.2017@smu.edu.sg>
2020-03-14 15:10:53 +0800

    test

--- flasktele/app.py
+++ flasktele/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
+from datetime import date
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vxfxezrkeewvvl:c7c1e9d6ae9fbe9215aeec5d8b2786e06372198925a4a8c877e8157137d1beef@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d9r73j1seev6bj'
@@ -12,9 +14,22 @@ maindb = db.Table('maindb', db.metadata, autoload=True, autoload_with=db.engine)
 # Base.prepare(db.engine, reflect=True)
 # maindb = Base.classes.maindb
 
+# @app.route("/query_test/", methods=["GET"])
+# def reply_test():
+#     name = request.json["name"]
+#     timestap  =
+#     print(name)
+#     if name:
+#         test = db.session.query(maindb).filter(maindb.c.mac_address=="8b20d37d47b52b298444971325218e759b516057").first()
+#         print(test[3])
+#         return (jsonify({"result": test[3]}), 201)
+#     else:
+#         return (jsonify({"result": "it works"}), 200)
+
 @app.route("/query_test/", methods=["GET"])
 def reply_test():
-    name = request.json["name"]
+    name = request.json["location"]
+    timestap  =
     print(name)
     if name:
         test = db.session.query(maindb).filter(maindb.c.mac_address=="8b20d37d47b52b298444971325218e759b516057").first()
@@ -22,19 +37,6 @@ def reply_test():
         return (jsonify({"result": test[3]}), 201)
     else:
         return (jsonify({"result": "it works"}), 200)
-
-# @app.route("/query_test/", methods=["GET"])
-# def reply(query_time):
-#     name = request.json["name"]
-#     print(name)
-#     if name:
-#         end_time = query_time
-#         start_time = ''
-#         #test = db.session.query(maindb).filter(maindb.c.mac_address=="8b20d37d47b52b298444971325218e759b516057").first()
-#         print(test[3])
-#         return (jsonify({"result": test[3]}), 201)
-#     else:
-#         return (jsonify({"result": "it works"}), 200) 
 
 @app.route('/')
 def index():
